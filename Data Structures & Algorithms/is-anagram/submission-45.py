00e9f110e163d1ed5db45979d69aedb422bbefeb
Dhiwahar Adhithya Kennady <adhithyak99@gmail.com>
2026-05-19 02:19:11 -0400

    Add: is-anagram - submission-45

---
+++ Data Structures & Algorithms/is-anagram/submission-45.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        
+        x = {}
+        for i in s:
+            if i in x:
+                x[i] += 1
+            else:
+                x[i] = 1
+
+        y = {}
+        for i in t:
+            if i in y:
+                y[i] += 1
+            else:
+                y[i] = 1
+
+
+        for i in x:
+            if i not in y:
+                return False
+            if x[i] != y[i]:
+                return False
+
+        return True
